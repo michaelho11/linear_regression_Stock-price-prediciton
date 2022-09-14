d065516d69802a00b44b2c6531fd4ccae40650e7
Michael <michaelho1156@gmail.com>
2022-09-14 08:40:26 +0800

    Input the judgement system, showing the prediction is correct or incorrect

--- main.py
+++ main.py
@@ -12,16 +12,14 @@ df = df.dropna()
 
 #df['SMA_3'] = df['Close'].rolling(window=3).mean()
 #df['SMA_10'] = df['Close'].rolling(window=10).mean()
-df['EMA_10'] = df['Close'].ewm(span=10).mean()
+df['EMA_10'] = df['Close'].ewm(span=3).mean()
 X = df[['EMA_10']]
 Y = df[['Close']]
 
 t=.8
 t = int(t*len(df))
-# Train dataset
 X_train = X[:t]
 Y_train = Y[:t]
-# Test dataset
 X_test = X[t:]
 Y_test = Y[t:]
 
@@ -29,10 +27,21 @@ linear = LinearRegression().fit(X_train,Y_train)
 predicted_price = linear.predict(X_test)
 predicted_price = pd.DataFrame(predicted_price,index=Y_test.index,columns = ['Predicted_price'])
 df['Predicted_price'] = predicted_price
-pred = df.dropna()
-print (pred[['Open','Close','Predicted_price']].to_string())
+df = df.dropna()
 
-# df.loc[(df[['Open']]<df[['Predicted_price']])&(df[['Open']]<df[['Close']]), 'Result'] == 'True'
+conditions = [(df['Predicted_price'] > df['Open'])&(df['Close'] > df['Open']),
+              (df['Predicted_price'] > df['Open'])&(df['Close'] < df['Open']),
+              (df['Predicted_price'] < df['Open'])&(df['Close'] < df['Open']),
+              (df['Predicted_price'] < df['Open'])&(df['Close'] > df['Open'])]
+choices = ['Correct', 'Incorrect', 'Correct', 'Incorrect']
+
+df ['Result'] = np.select(conditions, choices, default = 'null')
+# df.loc[(df['Predicted_price'] > df['Open'])&(df['Close'] > df['Open']),'Result'] = 'Match'
+# df.loc[(df['Predicted_price'] > df['Open'])&(df['Close'] < df['Open']),'Result'] = 'Mismatch'
+# df.loc[(df['Predicted_price'] < df['Open'])&(df['Close'] < df['Open']),'Result'] = 'Match'
+# df.loc[(df['Predicted_price'] < df['Open'])&(df['Close'] > df['Open']),'Result'] = 'Mismatch'
+
+#print (df)
 
 # predicted_price.plot(figsize=(10,5))
 # y_test.plot()
