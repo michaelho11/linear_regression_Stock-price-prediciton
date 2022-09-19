d14fa60ae3a77d4c1c7bdcca56419b5f0a2280b8
Michael <michaelho1156@gmail.com>
2022-09-19 11:05:05 +0800

    Use EMA_3 to predict the trend of the stock price

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# This program is to predict the trend of single trading day but not consecutive trading day
+# Predicting the trend by means of using ema_3
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,15 +8,16 @@ import yfinance as yf
 import sklearn
 from sklearn.linear_model import LinearRegression
 
-df = yf.download('2013.HK', start = '2022-01-01')
+df = yf.download('0175.HK', start = '2022-01-01')
 
 df = df[['Close','Open']]
 df = df.dropna()
 
 #df['SMA_3'] = df['Close'].rolling(window=3).mean()
-#df['SMA_10'] = df['Close'].rolling(window=10).mean()
+# df['SMA_10'] = df['Close'].rolling(window=10).mean()
 df['EMA_10'] = df['Close'].ewm(span=3).mean()
 X = df[['EMA_10']]
+#X = df[['SMA_10']]
 Y = df[['Close']]
 
 t=.8
@@ -25,18 +29,25 @@ Y_test = Y[t:]
 
 linear = LinearRegression().fit(X_train,Y_train)
 predicted_price = linear.predict(X_test)
+print(predicted_price)
+print('----')
+print(Y_test)
 predicted_price = pd.DataFrame(predicted_price,index=Y_test.index,columns = ['Predicted_price'])
 df['Predicted_price'] = predicted_price
 df = df.dropna()
+
+trending = [(df['Predicted_price'] < df['Open']),(df['Predicted_price'] > df['Open'])]
+choices1 = ['Sell','Buy']
+df['Trend'] = np.select(trending, choices1, default ='null')
 
 conditions = [(df['Predicted_price'] > df['Open'])&(df['Close'] > df['Open']),
               (df['Predicted_price'] > df['Open'])&(df['Close'] < df['Open']),
               (df['Predicted_price'] < df['Open'])&(df['Close'] < df['Open']),
               (df['Predicted_price'] < df['Open'])&(df['Close'] > df['Open'])]
-choices = ['Correct', 'Incorrect', 'Correct', 'Incorrect']
+choices2 = ['Correct', 'Incorrect', 'Correct', 'Incorrect']
 
-df ['Result'] = np.select(conditions, choices, default = 'null')
-# df.loc[(df['Predicted_price'] > df['Open'])&(df['Close'] > df['Open']),'Result'] = 'Match'
+df ['Result'] = np.select(conditions, choices2, default = 'null')
+# # df.loc[(df['Predicted_price'] > df['Open'])&(df['Close'] > df['Open']),'Result'] = 'Match'
 # df.loc[(df['Predicted_price'] > df['Open'])&(df['Close'] < df['Open']),'Result'] = 'Mismatch'
 # df.loc[(df['Predicted_price'] < df['Open'])&(df['Close'] < df['Open']),'Result'] = 'Match'
 # df.loc[(df['Predicted_price'] < df['Open'])&(df['Close'] > df['Open']),'Result'] = 'Mismatch'
